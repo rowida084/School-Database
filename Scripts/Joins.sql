08dc78229ff8eece43ef8493f6be3bfcfe85fbe9
Rowida Hany <rowidahany084@gmail.com>
2026-09-11 04:16:20 +0300

    Update Joins.sql

--- Scripts/Joins.sql
+++ Scripts/Joins.sql
@@ -2,3 +2,11 @@
 select stud_ID,stud_Name,dept_Name
 from Students inner join Departments 
 on Students.dept_ID=Departments.dept_ID
+
+
+--Get every Teacher who teaches more than 3 Courses using JOIN, GROUP BY, and HAVING 
+select tech_Name ,count(course_Name) as course_count
+from Teachers inner join Courses
+on tech_ID=teacher_ID
+group by tech_Name 
+having count(course_Name)>3
