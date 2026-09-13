72d759c1a16e2778701984f793be9f3784765d57
Rowida Hany <rowidahany084@gmail.com>
2026-09-13 12:38:22 +0300

    Add vw_DepartmentSummary view for department statistics
    Create a view to summarize department statistics including student and teacher counts.

--- Scripts/View.sql
+++ Scripts/View.sql
@@ -1,1 +1,13 @@
 
+--Create vw_DepartmentSummary showing each Department with Student Count and Teacher Count.
+create view vw_DepartmentSummary 
+as select d.dept_ID,d.dept_Name ,
+count(distinct stud_ID) as StudentCount ,
+count (distinct tech_ID) as TeacherCount 
+from Students s inner join Departments d 
+on s.dept_ID=d.dept_ID
+inner join Teachers t 
+on t.dept_ID=d.dept_ID
+group by d.dept_ID,d.dept_Name
+
+select * from vw_DepartmentSummary
