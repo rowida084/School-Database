86078025773bfe11516a44295a50d260a3377eec
Rowida Hany <rowidahany084@gmail.com>
2026-09-13 23:28:01 +0300

    Create vw_StudentFullReport view for student data
    Created a view to combine student information with course enrollment and grades, including a computed status based on the grade.

--- Scripts/View.sql
+++ Scripts/View.sql
@@ -22,3 +22,19 @@ group by tech_ID,tech_Name
 
 select * from vw_TeacherCourseLoad
 
+
+--Create vw_StudentFullReport combining Student information, enrolled Courses, Grades, and a computed Status.
+create view vw_StudentFullReport 
+as select s.stud_ID,s.stud_Name,
+c.course_ID,c.course_Name ,e.Grade,
+case 
+when e.Grade is null then 'Pending'
+when e.Grade >=50 then 'Passed'
+else 'Failed'
+end as status
+from Students s inner join Enrollment e
+on s.stud_ID = e.Student_ID
+inner join Courses c
+on c.course_ID = e.Course_ID
+
+select * from vw_StudentFullReport
