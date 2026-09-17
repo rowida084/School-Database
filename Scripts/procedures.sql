d8709ca954549673b491e8a5f7637614c04c5a15
Rowida Hany <rowidahany084@gmail.com>
2026-09-17 07:38:00 +0300

    Update procedures.sql

--- Scripts/procedures.sql
+++ Scripts/procedures.sql
@@ -69,3 +69,57 @@ sp_EnrollStudent 1 ,899--course not exist
 sp_EnrollStudent 1111,5--student not exist
 
 
+  --Create sp_TransferStudent(@StudentId, @NewDepartmentId).
+create proc sp_TransferStudent @StudentID int , @NewDepartment_ID int 
+as 
+begin 
+if not  exists
+(
+select 1 from Students where stud_ID=@StudentID
+)
+
+begin 
+select 'Student is not exist.'
+return
+end 
+if not exists 
+(
+select 1 from Departments where dept_ID=@NewDepartment_ID
+)
+begin 
+select 'Department is not exist.'
+return
+end 
+begin try 
+begin Transaction 
+if exists 
+(
+select 1 
+from Enrollment e inner join Courses c
+on e.Course_ID= c.course_ID
+where e.Student_ID = @StudentID and 
+c.department_ID <> @NewDepartment_ID
+)
+begin 
+rollback
+select 'Transfer cannot be completed because of Enrollment conflict.'
+return 
+end
+update Students
+set dept_ID =@NewDepartment_ID
+where stud_ID=@StudentID
+commit 
+ SELECT 'Student transferred successfully.'
+end try
+begin catch 
+if @@TRANCOUNT>0
+rollback
+end catch
+end 
+
+sp_TransferStudent 1,2--conflict
+insert into Students (stud_Name,dept_ID)
+values ('Rowida Hany',3)--new student in  department 3 with id 14
+sp_TransferStudent 14,2 --transfer done successfully
+sp_TransferStudent 14,5--this department is not exist in my database
+sp_TransferStudent 15,2 --this student is not exist 
